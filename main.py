33780cf184ad4aafebf1b320b2960b8a74b17ec9
Nikolle Vizcaya <17611684-NikolleVizcaya@users.noreply.replit.com>
2024-03-19 12:53:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+### Example Code 1
+
+fName = "Mr"
+lName = "Colley"
+
+print(fName)
+
+# Identify a variable in the code
+ # Answer:fName and lName
+
+# Identify a string in the code
+ # Answer:Mr and Colley
+
+# Line 4 is changed to lName = "Thorpe".  How does this affect the output?
+ #Answer When it is first ran Colley will show up but when is ran again after the change the output will change to Thorpe
+
+# Line 3 is changed to fName = "Mrs". How does this affect the output?
+ # Answer When it is first ran Mr will show up but when is ran again after the change the output will change to Mrs
+
+
+### Example Code 2
+
+num1 = 20
+num2 = 5
+
+total1 = num1 + 15
+total2 = num2 * 2
+total3 = num1 - num2
+
+print(total3)
+
+# What will be output by the program?
+ # Answer 15 since 20-5
+
+
+### Example Code 3
+
+name1 = "Ross" 
+name2 = "Monica" 
+name3 = "Joey" 
+name4 = "Rachel" 
+name5 = "Chandler" 
+ 
+print(name1 + " and " + name4) 
+print(name3) 
+ 
+name3 = "Phoebe" 
+
+
+# How many variables are used in the program?
+ # Answer only three variables are used 
+
+# What would be the impact of changing  print(name1, " and ", name4) to print(name1, " and ", name5) ?
+ # Answer Ross and Chandler will show up instead of Ross and Rachel
+
+# What is the purpose of the '+' symbol in  print(name1, " and ", name4) 
+ # Answer to add a string 
+
+# The line  print(name3) is added to the end of the code.  Explain what it will do.
+ # Answer it is replacing the orignal one so the second one is the one that will show up
